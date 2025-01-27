f990eea7536b640f3e2de6485145d873453934ee
vlad <Vladislavgopienko10@gmail.com>
2025-01-27 13:43:48 +0600

    Добавил импорт модели Like

--- source/webapp/models/__init__.py
+++ source/webapp/models/__init__.py
@@ -2,3 +2,4 @@ from webapp.models.base_model import BaseModel
 from webapp.models.article import Article
 from webapp.models.comment import Comment
 from webapp.models.tag import Tag
+from webapp.models.like import Like
